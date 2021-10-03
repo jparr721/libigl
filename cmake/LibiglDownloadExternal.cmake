21744f25ca32318789dc8f8a2776ad2f9171bff4
Jarred Parr <jparr721@gmail.com>
2021-10-03 13:52:30 -0600

    patch eigen download

--- cmake/LibiglDownloadExternal.cmake
+++ cmake/LibiglDownloadExternal.cmake
@@ -67,7 +67,7 @@ endfunction()
 set(LIBIGL_EIGEN_VERSION 3.3.7 CACHE STRING "Default version of Eigen used by libigl.")
 function(igl_download_eigen)
 	igl_download_project(eigen
-		GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+		GIT_REPOSITORY https://gitlab.com/cantonios/eigen.git
 		GIT_TAG        ${LIBIGL_EIGEN_VERSION}
 		${LIBIGL_BRANCH_OPTIONS}
 	)
